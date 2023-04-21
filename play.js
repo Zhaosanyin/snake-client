7127a37177d45eba522aaf818da22660949352b5
zhaosanyin <zhaosan.yin@gmail.com>
2023-04-21 04:16:59 +0000

    moving

--- play.js
+++ play.js
@@ -1,5 +1,5 @@
 //play.js // client.js
-const conn = require ('./client.js')
+const conn = require('./client.js');
 const net = require("net");
 // const conn = function(){
 //   const conn = net.createConnection({
@@ -14,15 +14,34 @@ const net = require("net");
 // console.log('Connecting ...');
 //  conn();
 
-// conn.on("data", (data)=>{
-//   console.log("Server says: ", data);
-// });
+
 
 const connection = conn();
 
 connection.on("connect", ()=>{
   console.log("Successfully connected to game server");
-})
+});
 connection.on("connect", ()=>{
   connection.write("Name: ZSY");
-})
+  
+});
+
+connection.on("connect", ()=>{
+  setTimeout(() => {
+    connection.write("Move: up");
+  }, 50);
+});
+const intervalId = setInterval(()=>{
+  connection.write("Move: up");
+}, 500);
+
+setTimeout(() => {
+  clearInterval(intervalId);
+}, 5000);
+
+
+// setTimeout(() => {
+//   clearInterval(timerId);
+//   console.log('Timer stopped');
+// }, 5000);
+
